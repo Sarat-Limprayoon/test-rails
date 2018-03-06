34f0c65b0289084bd43ad94d7f01e06e098c26a4
Sarat Limprayoon <sarat.limprayoon@rockyou.com>
2018-03-06 01:25:08 -0800

    Complete alpha blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -8,7 +13,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created."
-      redirect_to articles_path(@article)
+      redirect_to article_path(@article)
     else
       render :new
     end
@@ -16,6 +21,27 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated."
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    flash[:notice] = "Article was successfully deleted."
+    redirect_to articles_path
   end
 
   private
